c3067f79b0bff8512182de4fc2dff250b226f3e2
ZaripovT <timurfromcanada@gmail.com>
2023-01-11 21:25:07 +0500

    Starting of project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambdaOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
